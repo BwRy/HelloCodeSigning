eb5ea533cb9a57e1d698f2f8b7881321e9fe6aba
Stefan Arentz (Mozilla) <sarentz@Copernicus.local>
2015-02-22 23:12:27 -0500

    Initial Commit

---
+++ HelloCodeSigning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloCodeSigning
+//
+//  Created by Stefan Arentz (Mozilla) on 2015-02-22.
+//  Copyright (c) 2015 Stefan Arentz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
